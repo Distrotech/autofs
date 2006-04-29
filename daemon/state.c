8a1101d53707b0ed2efc086cee190ed9f9c2e79d
Ian Kent <raven@raven.themaw.net>
2006-04-29 19:49:06 +0800

    - set stack size for state queue handler thread.

--- daemon/state.c
+++ daemon/state.c
@@ -870,9 +870,21 @@ static void *st_queue_handler(void *arg)
 int st_start_handler(void)
 {
 	pthread_t thid;
+	pthread_attr_t attrs;
+	pthread_attr_t *pattrs = &attrs;
 	int status;
 
-	status = pthread_create(&thid, &thread_attr, st_queue_handler, NULL);
+	status = pthread_attr_init(pattrs);
+	if (status)
+		pattrs = NULL;
+	else {
+		pthread_attr_setdetachstate(pattrs, PTHREAD_CREATE_DETACHED);
+#ifdef _POSIX_THREAD_ATTR_STACKSIZE
+		pthread_attr_setstacksize(pattrs, PTHREAD_STACK_MIN*4);
+#endif
+	}
+
+	status = pthread_create(&thid, pattrs, st_queue_handler, NULL);
 	if (status)
 		return 0;
 
