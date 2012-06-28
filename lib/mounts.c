a82b8f1665e28ccdbde1ae7c75f0e55b45c012ce
Ian Kent <ikent@redhat.com>
2012-06-28 19:13:26 +0800

    autofs-5.0.6 - fix remount of multi mount
    Went accessing a multi-mount only the the offsets that need to be mounted
    are mounted. But when re-mounting multi-mounts during a restart we need to
    also traverse into existing mounts and re-connect to triggers mounted within
    them.

--- lib/mounts.c
+++ lib/mounts.c
@@ -1661,6 +1661,21 @@ int mount_multi_triggers(struct autofs_point *ap, struct mapent *me,
 			goto cont;
 
 		mounted += do_mount_autofs_offset(ap, oe, root, offset);
+
+		/*
+		 * If re-constructing a multi-mount it's necessary to walk
+		 * into nested mounts, unlike the usual "mount only what's
+		 * needed as you go" behavior.
+		 */
+		if (ap->state == ST_READMAP && ap->flags & MOUNT_FLAG_REMOUNT) {
+			if (oe->ioctlfd != -1 ||
+			    is_mounted(_PROC_MOUNTS, oe->key, MNTS_REAL)) {
+				char oe_root[PATH_MAX + 1];
+				strcpy(oe_root, root);
+				strcat(oe_root, offset); 
+				mount_multi_triggers(ap, oe, oe_root, strlen(oe_root), base);
+			}
+		}
 cont:
 		offset = cache_get_offset(base,
 				offset, start, &me->multi_list, &pos);
