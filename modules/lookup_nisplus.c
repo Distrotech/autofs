e8fb238a62dea118d47435394d484a1714a779b2
Ian Kent <raven@themaw.net>
2013-02-12 12:00:38 +0800

    autofs-5.0.7 - fix wildcard multi map regression
    A recent patch that removed code to add the current map entry when
    being parsed if it didn't already exist cause wildcard indirect
    multi-mount map entries to fail to mount.
    
    Indirect multi-mount map entries need the entry matched by a wildcard
    lookup to be added to the map entry cache because subsequent operations
    expect a distinct map entry to be present or they will fail. This is
    what the code that was removed did but it did so in the wrong place
    which caused a deadlock situation.

--- modules/lookup_nisplus.c
+++ modules/lookup_nisplus.c
@@ -561,7 +561,7 @@ int lookup_mount(struct autofs_point *ap, const char *name, int name_len, void *
 			return status;
 	}
 
-	cache_readlock(mc);
+	cache_writelock(mc);
 	me = cache_lookup(mc, key);
 	/* Stale mapent => check for entry in alternate source or wildcard */
 	if (me && !me->mapent) {
@@ -571,10 +571,21 @@ int lookup_mount(struct autofs_point *ap, const char *name, int name_len, void *
 		if (!me)
 			me = cache_lookup_distinct(mc, "*");
 	}
-	if (me && me->mapent && (me->source == source || *me->key == '/')) {
-		mapent_len = strlen(me->mapent);
-		mapent = malloc(mapent_len + 1);
-		strcpy(mapent, me->mapent);
+	if (me && me->mapent) {
+		/*
+		 * Add wildcard match for later validation checks and
+		 * negative cache lookups.
+		 */
+		if (ap->type == LKP_INDIRECT && me->key == '*') {
+			ret = cache_update(mc, source, key, me->mapent, me->age);
+			if (!(ret & (CHE_OK | CHE_UPDATED)))
+				me == NULL;
+		}
+		if (me && (me->source == source || *me->key == '/')) {
+			mapent_len = strlen(me->mapent);
+			mapent = malloc(mapent_len + 1);
+			strcpy(mapent, me->mapent);
+		}
 	}
 	cache_unlock(mc);
 
