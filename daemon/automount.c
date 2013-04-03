82e54eb867f58368173354e05177b7ca091c4490
Ian Kent <ikent@redhat.com>
2013-04-03 16:27:46 +0800

    Refresh of debug.patch

--- daemon/automount.c
+++ daemon/automount.c
@@ -1343,7 +1343,7 @@ static void *statemachine(void *arg)
 		case SIGUSR2:
 			master_mutex_lock();
 			if (list_empty(&master_list->completed)) {
-				if (list_empty(&master_list->mounts)) {
+				if (!list_empty(&master_list->mounts)) {
 					debug(LOGOPT_ANY, "list empty completed and mounts");
 					master_mutex_unlock();
 					return NULL;
