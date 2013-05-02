b3b44a4585e57a925e409bcaef4685698cbfab33
Ian Kent <raven@themaw.net>
2013-05-02 16:13:27 +0800

    Refresh of autofs-5.0.7-use-MS_UNBINDABLE-for-autofs-mounts.patch

--- daemon/direct.c
+++ daemon/direct.c
@@ -26,12 +26,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <sys/mount.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/poll.h>
+#include <sys/mount.h>
 #include <sys/vfs.h>
 #include <sched.h>
 
