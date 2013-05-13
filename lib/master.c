0174b24fd2b061de5fc39a568628a0c02610f5e8
Ian Kent <raven@themaw.net>
2013-05-13 13:03:03 +0800

    Refresh of autofs-5.0.7-add-enable-sloppy-mount-option-to-configure.patch

--- lib/master.c
+++ lib/master.c
@@ -1320,12 +1320,6 @@ int master_show_mounts(struct master *master)
 
 		printf("\nMount point: %s\n", ap->path);
 
-		/* Report nulled nulled entries */
-		if (cache_lookup_distinct(master->nc, ap->path)) {
-			printf("\nmap entry %s nulled by previous entry\n",
-			       ap->path);
-		}
-
 		printf("\nsource(s):\n");
 
 		/*
