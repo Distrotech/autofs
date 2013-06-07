0f6faa82647d7eec356de4c8237ad363fcc3a227
Ian Kent <raven@themaw.net>
2013-06-07 13:54:07 +0800

    autofs-5.0.7 - fix potential null dereference in lookup_mount()
    Setting rv to CHE_OK by default could allow a null map entry dereference in
    lookup_mount() in several modules.

--- modules/lookup_yp.c
+++ modules/lookup_yp.c
@@ -699,7 +699,7 @@ int lookup_mount(struct autofs_point *ap, const char *name, int name_len, void *
 					       mapent, ctxt->parse->context);
 		if (ret) {
 			time_t now = time(NULL);
-			int rv = CHE_OK;
+			int rv = CHE_FAIL;
 
 			cache_writelock(mc);
 			me = cache_lookup_distinct(mc, key);
