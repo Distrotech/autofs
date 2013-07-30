d3f7dd963c0b24e6f140468e51e78639a055ea2f
Ian Kent <raven@themaw.net>
2013-07-30 10:03:58 +0800

    autofs-5.0.7 - add dumpmap to file
    The dumpmaps option doesn't allow maps to be output in <key, value>
    pairs suitable for use as a file map.
    
    This could be useful to save current maps as a backup for emergency
    use.
    
    If the dumpmaps option is given and there is an argument it is taken
    as a comma seperated list of map names to be output. The given names
    must match a existing map name and if so, and the map is available, it
    is output to a file of that name unless such a file already exists. If
    a file of the name of the map already exists then the map is ignored.

--- lib/master.c
+++ lib/master.c
@@ -1329,7 +1329,88 @@ static void print_map_info(struct map_source *source)
 	return;
 }
 
-int master_show_mounts(struct master *master)
+static char *match_map_path(const char *match, const char *maps)
+{
+	char *names;
+	char *tok, *ptr;
+	unsigned int found;
+
+	names = strdup(maps);
+	if (!names) {
+		printf("failed to allocate working storage,\n");
+		printf("map %s ignored\n", match);
+		return 0;
+	}
+
+	found = 0;
+	ptr = NULL;
+	tok = strtok_r(names, ",", &ptr);
+	while (tok) {
+		if (strcmp(match, tok)) {
+			tok = strtok_r(NULL, ",", &ptr);
+			continue;
+		}
+		found = 1;
+		break;
+	}
+
+	free(names);
+
+	if (!found) {
+		printf("map \"%s\" not found in \"%s\", not output\n",
+			match, maps);
+		return 0;
+	}
+
+	return 1;
+}
+
+static void write_map(const char *map, struct mapent *first)
+{
+	struct mapent *me = first;
+	char *name, *out;
+	FILE *f;
+
+	/* map has no name (eg. hosts) */
+	if (!map) {
+		printf("  map has no name, not output\n");
+		return;
+	}
+
+	name = strdup(map);
+	if (!name) {
+		printf("  failed to allocate working storage, "
+		       "map %s not output\n", map);
+		return;
+	}
+
+	if (strchr(name, '/'))
+		out = basename(name);
+	else
+		out = name;
+
+	f = open_fopen_wx(out);
+	if (!f) {
+		printf("  failed to open output file %s: %s\n",
+			out, strerror(errno));
+		printf("  map file not created.\n");
+		free(name);
+		return;
+	}
+
+	do {
+		fprintf(f, "%s\t%s\n", me->key, me->mapent);
+	} while ((me = cache_lookup_next(first->mc, me)));
+
+	printf("  output map %s\n", out);
+
+	fclose(f);
+	free(name);
+
+	return;
+}
+
+int master_show_mounts(struct master *master, const char *maps)
 {
 	struct list_head *p, *head;
 
@@ -1359,6 +1440,7 @@ int master_show_mounts(struct master *master)
 		struct autofs_point *ap;
 		time_t now = time(NULL);
 		unsigned int count = 0;
+		unsigned int matched = 0;
 		int i;
 
 		this = list_entry(p, struct master_mapent, list);
@@ -1367,6 +1449,14 @@ int master_show_mounts(struct master *master)
 		ap = this->ap;
 
 		printf("\nMount point: %s\n", ap->path);
+
+		if (maps) {
+			matched = match_map_path(ap->path, maps);
+			if (!matched) {
+				printf("\n");
+				continue;
+			}
+		}
 
 		printf("\nsource(s):\n");
 
@@ -1411,17 +1501,22 @@ int master_show_mounts(struct master *master)
 					       " will be ignored at run time\n");
 			}
 
-			printf("\n");
-
 			me = cache_lookup_first(source->mc);
 			if (!me)
 				printf("  no keys found in map\n");
 			else {
+				if (matched) {
+					write_map(source->argv[0], me);
+					goto next;
+				}
+
+				printf("\n");
+
 				do {
 					printf("  %s | %s\n", me->key, me->mapent);
 				} while ((me = cache_lookup_next(source->mc, me)));
 			}
-
+next:
 			count++;
 
 			source = source->next;
