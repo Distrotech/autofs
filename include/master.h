0899ad2df94560ed733b5a9ad7341051b96753f8
Ian Kent <raven@themaw.net>
2013-07-31 14:54:39 +0800

    autofs-5.0.7 - teach dumpmaps to output simple key value pairs

--- include/master.h
+++ include/master.h
@@ -112,6 +112,7 @@ int master_submount_list_empty(struct autofs_point *ap);
 int master_notify_submount(struct autofs_point *, const char *path, enum states);
 void master_notify_state_change(struct master *, int);
 int master_mount_mounts(struct master *, time_t, int);
+int dump_map(struct master *, const char *, const char *);
 int master_show_mounts(struct master *);
 extern inline unsigned int master_get_logopt(void);
 int master_list_empty(struct master *);
