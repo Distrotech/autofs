386f9cb6f1ec4944b0ac0e782f75204f7d0ed4b2
Ian Kent <raven@themaw.net>
2013-08-19 09:57:34 +0800

    debug.patch

--- modules/parse_sun.c
+++ modules/parse_sun.c
@@ -881,7 +881,8 @@ static int validate_location(unsigned int logopt, char *loc)
 		    !strncmp(ptr, "file:", 5) || !strncmp(ptr, "yp:", 3) ||
 		    !strncmp(ptr, "nis:", 4) || !strncmp(ptr, "nisplus:", 8) ||
 		    !strncmp(ptr, "ldap:", 5) || !strncmp(ptr, "ldaps:", 6) ||
-		    !strncmp(ptr, "sss:", 4) || !strncmp(ptr, "dir:", 4))
+		    !strncmp(ptr, "sss:", 4) || !strncmp(ptr, "dir:", 4) ||
+		    !strncmp(ptr, "hosts:", 4))
 			return 1;
 		error(logopt,
 		      "expected colon delimeter not found in location %s",
@@ -1293,6 +1294,15 @@ int parse_mount(struct autofs_point *ap, const char *name,
 	}
 
 	p = skipspace(pmapent);
+
+	/*
+	 * Change special map -hosts into <type>: format so it
+	 * doesn't confuse the options parse.
+	 */
+	if (strstr(p, "-hosts")) {
+		*p++ = 'h'; *p++ = 'o'; *p++ = 's'; *p++ = 't';
+		*p++ = ':';
+	}
 
 	/* Deal with 0 or more options */
 	if (*p == '-') {
