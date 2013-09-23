f982e5457dd9a621f6c1f7c9d2be233af361facf
Ian Kent <ikent@redhat.com>
2013-09-23 14:21:17 +0800

    Refresh of debug.patch

--- lib/rpc_subs.c
+++ lib/rpc_subs.c
@@ -345,17 +345,15 @@ static rpcprog_t rpc_getrpcbyname(const rpcprog_t program)
 	struct rpcent *entry;
 	unsigned int i;
 
-	if (table != NULL) {
-		pthread_mutex_lock(&rpcb_mutex);
-		for (i = 0; rpcb_pgmtbl[i] != NULL; i++) {
-			entry = getrpcbyname(rpcb_pgmtbl[i]);
-			if (entry) {
-				pthread_mutex_unlock(&rpcb_mutex);
-				return (rpcprog_t)entry->r_number;
-			}
+	pthread_mutex_lock(&rpcb_mutex);
+	for (i = 0; rpcb_pgmtbl[i] != NULL; i++) {
+		entry = getrpcbyname(rpcb_pgmtbl[i]);
+		if (entry) {
+			pthread_mutex_unlock(&rpcb_mutex);
+			return (rpcprog_t)entry->r_number;
 		}
-		pthread_mutex_unlock(&rpcb_mutex);
 	}
+	pthread_mutex_unlock(&rpcb_mutex);
 #endif
 	return program;
 }
