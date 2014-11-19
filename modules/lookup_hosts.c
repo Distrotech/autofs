554323a6305daec40d2cd262f1345eac3b0aac5d
Ian Kent <ikent@redhat.com>
2014-11-19 09:25:32 +0800

    autofs-5.1.0 - ensure negative cache isn't updated on remount
    The negative cache shouldn't be updated when re-connecting at
    startup but a couple of lookup modules didn't check for this
    case.

--- modules/lookup_hosts.c
+++ modules/lookup_hosts.c
@@ -151,6 +151,9 @@ static int do_parse_mount(struct autofs_point *ap, struct map_source *source,
 	if (ret) {
 		struct mapent_cache *mc = source->mc;
 
+		/* Don't update negative cache when re-connecting */
+		if (ap->flags & MOUNT_FLAG_REMOUNT)
+			return NSS_STATUS_TRYAGAIN;
 		cache_writelock(mc);
 		cache_update_negative(mc, source, name, ap->negative_timeout);
 		cache_unlock(mc);
